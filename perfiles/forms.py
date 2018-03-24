b917b5a07b9aa57a0025b67d3c14e3da724afa9b
msigxix <oscarsolis1981@gmail.com>
2018-03-23 20:15:38 -0500

    Agregado Estilos

--- perfiles/forms.py
+++ perfiles/forms.py
@@ -5,22 +5,15 @@ from django.contrib.auth.models import User
 from .models import Perfil
 
 class SignUpForm(UserCreationForm):
-    first_name = forms.CharField(max_length=140, required=True)
-    last_name = forms.CharField(max_length=140, required=False)
-    email = forms.EmailField(required=True)
+    username = forms.CharField(label='Usuario:', min_length=4, max_length=150,widget=forms.TextInput(attrs={'class': 'form-group valid-form has-error','placeholder': 'Escribir un usuario aquí'}))
+    first_name = forms.CharField(forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'First Name'}), max_length=32, help_text='First name')
+    last_name=forms.CharField(forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Last Name'}), max_length=32, help_text='Last name')
+    email=forms.EmailField(forms.EmailInput(attrs={'class': 'form-control', 'placeholder': 'Email'}), max_length=64, help_text='Enter a valid email address')
+    password1=forms.CharField(forms.PasswordInput(attrs={'class': 'form-control', 'placeholder': 'Password'}))
+    password2=forms.CharField(forms.PasswordInput(attrs={'class': 'form-control', 'placeholder': 'Password Again'}))
 
     class Meta:
         model = User
-        fields = [
-            'username',
-            'email',
-            'first_name',
-            'last_name',
-            'password1',
-            'password2',
-        ]
-        labels = {
-            'username': 'Nombre de Usuario',
-            'email': 'Correo Electronico',
-            'first_name': 'Nombre',
-        }
+        # I've tried both of these 'fields' declaration, result is the same
+        fields = ('username', 'first_name', 'last_name', 'email', 'password1', 'password2', )
+        #fields = UserCreationForm.Meta.fields + ('first_name', 'last_name', 'email',)
