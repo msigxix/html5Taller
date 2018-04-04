310122e0ae52b4cf503791bda38487babe03d6b6
msigxix <oscarsolis1981@gmail.com>
2018-04-03 21:02:47 -0500

    Se arregla registro de usuarios, luego de registrar va al login

--- usuarios/urls.py
+++ usuarios/urls.py
@@ -29,7 +29,7 @@ from perfiles.views import SignUpView, BienvenidaView, SignInView, SignOutView ,
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^$', BienvenidaView.as_view(template_name="perfiles/Registro.html"), name='bienvenida'),
+    url(r'^$', BienvenidaView.as_view(), name='bienvenida'),
     url(r'^registrate/$', SignUpView.as_view(), name='sign_up'),
     url(r'^inicia-sesion/$', SignInView.as_view(), name='sign_in'),
     #url(r'^inicia-sesion/$', login, {'template_name:iniciar_sesion.html'}, name='sign_in'),    
