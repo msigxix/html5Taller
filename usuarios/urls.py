cf430371eda3b567ea600b07ff1b0ded09bab37a
Lenovo-PC\Myris <myris3184@gmail.com>
2018-04-05 20:58:10 -0500

    Se modifica archivo base y urls.py agragando la ruta de la pagina de inicio.

--- usuarios/urls.py
+++ usuarios/urls.py
@@ -30,9 +30,7 @@ from perfiles.views import SignUpView, BienvenidaView, InicioView, SignInView, S
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^$', BienvenidaView.as_view(), name='bienvenida'),
-
     url(r'^inicio/$', InicioView.as_view(), name='inicio'),
-
     url(r'^registrate/$', SignUpView.as_view(), name='sign_up'),
     url(r'^inicia-sesion/$', SignInView.as_view(), name='sign_in'),
     #url(r'^inicia-sesion/$', login, {'template_name:iniciar_sesion.html'}, name='sign_in'),    
